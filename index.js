536ed629bd8ccb7c6315f2decdfc08e537332d9e
BenEngry <bazivolyakm@gmail.com>
2022-06-15 21:23:43 +0300

    added panel

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ arrPanelItems.forEach(element => {
     element.addEventListener("mouseout", () => {
         element.classList.remove("activePanel");
         element.childNodes[1].classList.remove("activePanelIcon");
+        element.childNodes[1].childNodes[1].classList.remove("filterColor");
+        const arrSvgIcons = [...element.childNodes[1].childNodes].filter(item => {return item.tagName == "IMG" });
+        arrSvgIcons.forEach(el => {
+            el.classList.remove("filterColor");
+        });
+
     });
 });
 arrPanelItemsIcon.forEach(element => {
     element.addEventListener("mouseover", () => {
         element.parentNode.classList.add("activePanel");
         element.classList.add("activePanelIcon");
+        const arrSvgIcons = [...element.childNodes].filter(item => {return item.tagName == "IMG" });
+        arrSvgIcons.forEach(el => {
+            el.classList.add("filterColor");
+        });
     });
 });
